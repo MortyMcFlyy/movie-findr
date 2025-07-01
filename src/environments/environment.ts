e8870ac9fbb3ef01a59bfd430da1c8ba3d6620ed
mortymcfly <felix.lukowski@hs-osnabrueck.de>
2025-07-01 16:22:23 +0200

    API KEY

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  tmbdApiKey:'815664ccc89e125e65be7c21c37dcb64'
+  
 };
 
 /*
