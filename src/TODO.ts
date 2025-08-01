4a90d64527bdf5daed1ea57dcdc0f98a3380485f
Verena Haarmeier <verenahaarmeier@web.de>
2025-08-01 14:22:17 +0200

    TODO

--- src/TODO.ts
+++ src/TODO.ts
@@ -1,26 +1,26 @@
 // TODO
-// Cards um favorisieren-Button ergänzen 
-// Cards um gesehen-Button ergänzen 
-// Availability auf Cards ergänzen
-// fuzzy search
 // lokale DB für die favoriten und history, statistiken, Zustimmung Geolocation
 // GeoLocation-Service (generate Befehl)
-// Accounteinstellungen ?
+// Accounteinstellungen ? zB FSK
 // Alles Hübsch machen
-
+// History page
+// Favoriten page
+// Favoriten und History in der DB speichern
 
 
 
 // Felix
 // Vibe searching algorithmus anpassen + Streamingprovider + Geolocation neu anfragen?
 // Letterbox API 
-
+// Cards um favorisieren-Button ergänzen 
+// Cards um gesehen-Button ergänzen 
+// Availability auf Cards ergänzen
 
 // Verena
 // Bei Genres --> auch noch andere Kategorien (Best of All Time, Alle Filme,.. )
 //     --> Filtermöglichkeiten (Filmlänge, FSK, gesehen?, Streamingprovider...) die über die Kategorien hinweg eingstellt bleiben
 // does the dog die API
-
+// fuzzy search
 
 
 
