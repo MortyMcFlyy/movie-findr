8fd4b9cb33aaf78d8b77d60b0fc6c6e0948e3e59
Felix Lukowski <felix.lukowski@web.de>
2025-08-11 14:34:53 +0200

    todo update

--- src/TODO.ts
+++ src/TODO.ts
@@ -13,8 +13,7 @@
 // Felix
 // Vibe searching algorithmus anpassen + Streamingprovider + Geolocation neu anfragen?
 // Letterbox API 
-// Cards um favorisieren-Button ergänzen 
-// Cards um gesehen-Button ergänzen 
+
 
 // Verena
 // Bei Genres --> auch noch andere Kategorien (Best of All Time, Alle Filme,.. )
@@ -24,4 +23,8 @@
 
 //DONE:
 //Availability auf Cards ergänzen
+
+
+// Cards um favorisieren-Button ergänzen ->persistieren in der DB
+// Cards um gesehen-Button ergänzen -> persistieren in der DB
 
